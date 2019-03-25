7dfc4481d03c8a3a60fbd4e603e39e51454bb166
Nat <natalie.hoang91@gmail.com>
2019-03-25 14:58:17 +0700

    modal show trailer

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-input-range/lib/css/index.css';
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-input-range/lib/css/index.css";
 
-
-import MoviesList from './MoviesList';
-import SearchMovies from './SearchMovies';
-import RangeFilterSection from './RangeFilterSection';
-import GenreSelect from './GenreSelect';
+import MoviesList from "./MoviesList";
+import SearchMovies from "./SearchMovies";
+import RangeFilterSection from "./RangeFilterSection";
+import GenreSelect from "./GenreSelect";
 import PaginationComponent from "react-reactstrap-pagination";
 
-
-
-
 class App extends Component {
-
   constructor() {
     super();
     this.state = {
       results: [],
-      filterText: '',
-      filterYear: { min: 0, max: 10, },
-      filterRating: { min: 0, max: 10, },
+      filterText: "",
+      filterYear: { min: 0, max: 10 },
+      filterRating: { min: 0, max: 10 },
       genresList: [],
-      chosenGenre: '',
-      orYear: { orMin: 0, orMax: 10},
-      orRating: { orMin: 0, orMax: 10},
-      selectedPage: 1,
-    }
+      chosenGenre: "",
+      orYear: { orMin: 0, orMax: 10 },
+      orRating: { orMin: 0, orMax: 10 },
+      selectedPage: 1
+    };
   }
 
   componentDidMount = () => {
@@ -35,85 +30,97 @@ class App extends Component {
     this.getGenres();
     //get the movies list from api.
     this.getMoviesSpecs();
-  }
+  };
 
   //this function fetch movies from api.
   getMoviesSpecs = () => {
     const API_KEY = "b2c1160c5fb292502b2d762fc2485fd3";
-    const api = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${this.state.selectedPage}&with_genres=${this.state.chosenGenre}`;
+    const api = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${
+      this.state.selectedPage
+    }&with_genres=${this.state.chosenGenre}`;
 
     fetch(api)
-    .then(response => response.json())
-    .then(response => response.results)
-    .then(movies => {
-      this.setState({
-        //set results to movies array.
-        results: movies,
+      .then(response => response.json())
+      .then(response => response.results)
+      .then(movies => {
+        this.setState({
+          //set results to movies array.
+          results: movies,
 
-        //set Year and Rating for range filter function below.
-        filterYear: {
-          min: Math.min(...movies.map(movie => movie.release_date.substring(0, 4))),
-          max: Math.max(...movies.map(movie => movie.release_date.substring(0, 4))),
-        },
-        filterRating: {
-          min: Math.min(...movies.map(movie => movie.vote_average)),
-          max: Math.max(...movies.map(movie => movie.vote_average)),
-        },
-        orYear: {
-          orMin: Math.min(...movies.map(movie => movie.release_date.substring(0, 4))),
-          orMax: Math.max(...movies.map(movie => movie.release_date.substring(0, 4))),
-        },
-        orRating: {
-          orMin: Math.min(...movies.map(movie => movie.vote_average)),
-          orMax: Math.max(...movies.map(movie => movie.vote_average)),
-        },
-        
+          //set Year and Rating for range filter function below.
+          filterYear: {
+            min: Math.min(
+              ...movies.map(movie => movie.release_date.substring(0, 4))
+            ),
+            max: Math.max(
+              ...movies.map(movie => movie.release_date.substring(0, 4))
+            )
+          },
+          filterRating: {
+            min: Math.min(...movies.map(movie => movie.vote_average)),
+            max: Math.max(...movies.map(movie => movie.vote_average))
+          },
+          orYear: {
+            orMin: Math.min(
+              ...movies.map(movie => movie.release_date.substring(0, 4))
+            ),
+            orMax: Math.max(
+              ...movies.map(movie => movie.release_date.substring(0, 4))
+            )
+          },
+          orRating: {
+            orMin: Math.min(...movies.map(movie => movie.vote_average)),
+            orMax: Math.max(...movies.map(movie => movie.vote_average))
+          }
+        });
       });
-    })
-  }
+  };
 
   //this function fetch the list of genre name and id and put it in state.
   getGenres = () => {
     const api = `https://api.themoviedb.org/3/genre/movie/list?api_key=b2c1160c5fb292502b2d762fc2485fd3&language=en-US
     `;
     fetch(api)
-    .then(response => response.json())
-    .then(response => this.setState({
-      genresList: response.genres,
-    }
-    ));
-  }
+      .then(response => response.json())
+      .then(response =>
+        this.setState({
+          genresList: response.genres
+        })
+      );
+  };
 
   //this function set value of filterText according to the value inside search input.
-  handleFilterTextChange = (e) => {
+  handleFilterTextChange = e => {
     this.setState({
-      filterText: e.target.value,
+      filterText: e.target.value
     });
-  }
+  };
 
-  handleYearChange = (value) => {
+  handleYearChange = value => {
     this.setState({ filterYear: value });
-  }
+  };
 
-  handleRatingChange = (value) => {
+  handleRatingChange = value => {
     this.setState({ filterRating: value });
-  }
+  };
 
   //this function take the value from select input and set chosenGenre to it.
-  handleSelectGenre = (e) => {
-    this.setState({
-      chosenGenre: e.target.value,
-    }, this.getMoviesSpecs);
-  }
+  handleSelectGenre = e => {
+    this.setState(
+      {
+        chosenGenre: e.target.value
+      },
+      this.getMoviesSpecs
+    );
+  };
 
   //this function update the selected page
-  handleSelectPage = (selectedPage) => {
+  handleSelectPage = selectedPage => {
     this.setState({ selectedPage: selectedPage }, this.getMoviesSpecs);
-  }
+  };
 
   //start of render
   render() {
-
     const filterText = this.state.filterText;
 
     const filterYear = this.state.filterYear;
@@ -125,44 +132,53 @@ class App extends Component {
     const genresList = this.state.genresList;
 
     //this filter the fetched movies list base on filterText /filterYear /filterRating.
-    const movies = this.state.results.filter(x => 
-      x.title.toLowerCase().includes( filterText.toLowerCase() ) && (x.release_date.substring(0, 4) <= filterYear.max) && (x.release_date.substring(0, 4) >= filterYear.min) && (x.vote_average <= filterRating.max) && (x.vote_average >= filterRating.min)
+    const movies = this.state.results.filter(
+      x =>
+        x.title.toLowerCase().includes(filterText.toLowerCase()) &&
+        x.release_date.substring(0, 4) <= filterYear.max &&
+        x.release_date.substring(0, 4) >= filterYear.min &&
+        x.vote_average <= filterRating.max &&
+        x.vote_average >= filterRating.min
     );
 
-
     return (
-    <div>
+      <div>
+        <PaginationComponent
+          totalItems={1000}
+          pageSize={1}
+          onSelect={this.handleSelectPage}
+          maxPaginationNumbers={9}
+        />
 
-      <PaginationComponent
-        totalItems={1000}
-        pageSize={1}
-        onSelect={this.handleSelectPage}
-        maxPaginationNumbers={9}
-      />
+        <div className="container-fluid row">
+          <div className="col-md-3">
+            <div className="text-danger">Total movies: {movies.length}</div>
 
-      <div className="container-fluid row">
-        <div className="col-md-3">
-        
-          <div className="text-danger">Total movies: {movies.length}</div>
+            <GenreSelect
+              genresList={genresList}
+              onSelectGenre={this.handleSelectGenre}
+            />
 
-          <GenreSelect genresList={genresList} onSelectGenre={this.handleSelectGenre} />
+            <SearchMovies onSearch={this.handleFilterTextChange} />
 
-          <SearchMovies onSearch={this.handleFilterTextChange} />
-
-          <RangeFilterSection onFilterYear={value => this.handleYearChange(value)} onFilterRating={value => this.handleRatingChange(value)} orYear={orYear} filterYear={filterYear} orRating={orRating} filterRating={filterRating} />
-
+            <RangeFilterSection
+              onFilterYear={value => this.handleYearChange(value)}
+              onFilterRating={value => this.handleRatingChange(value)}
+              orYear={orYear}
+              filterYear={filterYear}
+              orRating={orRating}
+              filterRating={filterRating}
+            />
+          </div>
+          <MoviesList
+            filterText={filterText}
+            movies={movies}
+            filterYear={filterYear}
+          />
         </div>
-        <MoviesList filterText={filterText} movies={movies} filterYear={filterYear}/>
       </div>
-    </div>
-    );//end of return
-
-
-
-
-  }//end of render
-
-
+    ); //end of return
+  } //end of render
 }
 
 export default App;
--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,28 +1,116 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import ReactModal from "react-modal";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import YouTube from "@u-wave/react-youtube";
 
 export default class MovieCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      TrailerResult: [],
+      showModal: false,
+      VideoId: ""
+      // VideoKey:[]
+    };
+  }
+
+  getMoviesSpecs = async () => {
+    const API_KEY = "b2c1160c5fb292502b2d762fc2485fd3";
+    const URL = `https://api.themoviedb.org/3/movie/${
+      this.props.id
+    }/videos?api_key=${API_KEY}&language=en-US`;
+
+    let response = await fetch(URL);
+    let data = await response.json();
+    let result = data.results;
+
+    this.setState({
+      // VideoID: result.id,
+      TrailerResult: result
+    });
+  };
+
+  handleShowModal = () => {
+    this.setState({ showModal: true });
+    this.getMoviesSpecs();
+  };
+  handleCloseModal = () => {
+    this.setState({ showModal: false });
+  };
+
   render() {
+    // if (this.state.TrailerResult) {
+    //   let videoKey = this.state.TrailerResult.map(item =>
+    //     item.key
+    //   );
+    // }
+    console.log(this.props.id);
+    console.log(this.state.TrailerResult);
+
     const BASE_URL = "https://image.tmdb.org/t/p/w500";
 
     return (
       <div className="col-md-3 mb-5 movie-card">
         <div className="card">
-          <img className="card-img-top" src={BASE_URL + this.props.posterUrl} alt="Card cap" />
+          <img
+            className="card-img-top"
+            src={BASE_URL + this.props.posterUrl}
+            alt="Card cap"
+          />
           <div className="card-body">
             <h5 className="card-title">{this.props.title}</h5>
             <span className="d-flex justify-content-between">
               <span>
-                <p><small>Year</small></p>
-                <p><strong>{this.props.year.substring(0, 4)}</strong></p>
+                <p>
+                  <small>Year</small>
+                </p>
+                <p>
+                  <strong>{this.props.year.substring(0, 4)}</strong>
+                </p>
               </span>
               <span>
-                <p><small>Rating</small></p>
-                <p><strong>{this.props.rating}</strong></p>
+                <p>
+                  <small>Rating</small>
+                </p>
+                <p>
+                  <strong>{this.props.rating}</strong>
+                </p>
               </span>
             </span>
           </div>
+          <div className="card-footer text-center">
+            <button onClick={this.handleShowModal}> TRAILER </button>
+            <div
+              class="modal fade bd-example-modal-lg"
+              tabindex="-1"
+              role="dialog"
+              aria-labelledby="myLargeModalLabel"
+              aria-hidden="true"
+            >
+              <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                  <ReactModal
+                    isOpen={this.state.showModal}
+                    onRequestClose={this.handleCloseModal}
+                  >
+                    <h1>Helloo</h1>
+                    <YouTube
+                      video={
+                        this.state.TrailerResult[0]
+                          ? this.state.TrailerResult[0].key
+                          : "h6hZkvrFIj0"
+                      }
+                      autoplay
+                    />
+                  </ReactModal>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
