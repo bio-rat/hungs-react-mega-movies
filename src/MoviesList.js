ca979b06979574fb095f2300e44422ffcea5edd5
etudeofmemories8698 <etudeofmemories8698@gmail.com>
2019-03-25 17:22:55 +0700

    modal show trailer (#2)

--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import MovieCard from './MovieCard';
-
+import React, { Component } from "react";
+import MovieCard from "./MovieCard";
 
 export default class MoviesList extends Component {
   render() {
@@ -8,10 +7,19 @@ export default class MoviesList extends Component {
       <div className="col-md-9">
         <div className="row hidden-md-up">
           {this.props.movies.map(x => {
-            return <MovieCard key={x.id} posterUrl={x.poster_path} title={x.title} year={x.release_date} rating={x.vote_average} />;
+            return (
+              <MovieCard
+                key={x.id}
+                id={x.id}
+                posterUrl={x.poster_path}
+                title={x.title}
+                year={x.release_date}
+                rating={x.vote_average}
+              />
+            );
           })}
         </div>
       </div>
-    )
+    );
   }
 }
